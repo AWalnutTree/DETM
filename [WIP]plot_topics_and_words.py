d6154d6c3db3776f8fb68303025289677f4a5e70
Andy Wang <120694087+AWalnutTree@users.noreply.github.com>
2024-09-02 20:15:44 -0400

    plot topics and words fixes

--- [WIP]plot_topics_and_words.py
+++ [WIP]plot_topics_and_words.py
@@ -39,39 +39,40 @@ args = CLI.parse_args()
 use = 'JM' if args.corpus == 'JM' else 'JMR'
 
 if use == 'JM':
-    beta = scipy.io.loadmat('./results/detm_jm_K_30_Htheta_800_Optim_adam_Clip_0.0_ThetaAct_relu_Lr_0.001_Bsz_10_RhoSize_300_L_3_minDF_10_trainEmbeddings_1_beta.mat')['values']
+    alpha = scipy.io.loadmat('./results/detm_jm_K_30_Htheta_800_Optim_adam_Clip_0.0_ThetaAct_relu_Lr_0.001_Bsz_10_RhoSize_300_L_3_minDF_10_trainEmbeddings_1_alpha.mat')['values']
     timestamps = 'data/JM/split_paragraph_False/min_df_10/timestamps.pkl'
     data_file = 'data/JM/split_paragraph_False/min_df_10'
     emb_path = 'embeddings/JM/skipgram_emb_300d.txt'
     shift_value = 1936
 else:
-    beta = scipy.io.loadmat('./results/detm_jmr_K_30_Htheta_800_Optim_adam_Clip_0.0_ThetaAct_relu_Lr_0.001_Bsz_10_RhoSize_300_L_3_minDF_10_trainEmbeddings_1_beta.mat')['values']
+    alpha = scipy.io.loadmat('./results/detm_jmr_K_30_Htheta_800_Optim_adam_Clip_0.0_ThetaAct_relu_Lr_0.001_Bsz_10_RhoSize_300_L_3_minDF_10_trainEmbeddings_1_alpha.mat')['values']
     timestamps = 'data/JMR/split_paragraph_False/min_df_10/timestamps.pkl'
     data_file = 'data/JMR/split_paragraph_False/min_df_10'
     emb_path = 'embeddings/JMR/skipgram_emb_300d.txt'
     shift_value = 1963
-print('beta: ', beta.shape)
+print('alpha: ', alpha.shape)
 
 # Load timestamps
 with open(timestamps, 'rb') as f:
     timelist = pickle.load(f)
 
-# Number of topics (K), timestamps (T), vocabulary size (V)
-K, T, V = beta.shape
+# Number of topics (K), timestamps (T), embeddings (L)
+K, T, L = alpha.shape
 
 # Option 1: Flatten the time axis to analyze overall topic distribution across all time points
-beta_flat = beta.reshape(K, -1)  # Shape: K x (T * V)
+alpha_flat = alpha.reshape(K, -1)  # Shape: K x (T * V)
 
 #====================================================================================================#
 
 #words = ['firm', 'online', 'item', 'food']
-words = ['ad', 'parameter', 'firm', 'household', 'customer', 'attribute']
+words = ['ad', 'parameter', 'firm', 'household', 'customer', 'attribute', 'price', 'supplier', 'participation']
 
 with open(f'{data_file}/vocab.pkl', 'rb') as f:
     vocab = pickle.load(f)
 
 vocab_size = len(vocab)
 embeddings = np.zeros((vocab_size, 300))
+MULTIPLIER = 1
 
 with open(emb_path, 'rb') as f:
     for l in f:
@@ -79,7 +80,7 @@ with open(emb_path, 'rb') as f:
         word = line[0]
         if word in vocab:
             vect = np.array(line[1:]).astype(float)
-            embeddings[vocab.index(word)] = vect
+            embeddings[vocab.index(word)] = vect * MULTIPLIER
 
 words_to_plot = words
 
@@ -101,11 +102,15 @@ words_to_plot = words
 
 #====================================================================================================#
 
+
+# print embedding of topic 
+print('embeddings: ', embeddings.shape)
+
 if args.list == []:
     if args.dim == '2':
         # Apply PCA
         pca = PCA(n_components=2)
-        beta_pca = pca.fit_transform(beta_flat)
+        beta_pca = pca.fit_transform(alpha_flat)
         
         pca_result = pca.fit_transform(embeddings)
 
@@ -129,7 +134,7 @@ if args.list == []:
     else:
         # Apply PCA
         pca = PCA(n_components=3)
-        beta_pca = pca.fit_transform(beta_flat)
+        beta_pca = pca.fit_transform(alpha_flat)
         pca_result = pca.fit_transform(embeddings)
 
         fig = plt.figure(figsize=(10, 8))
@@ -161,7 +166,7 @@ else:
             pca_results = np.zeros((T, 2))
             for t in range(T):
                 
-                pca_results[t, :] = pca.fit_transform(beta[:, t, :])[k]
+                pca_results[t, :] = pca.fit_transform(alpha[:, t, :])[k]
                 
 
             # Plot each topic's trajectory over time
@@ -196,7 +201,7 @@ else:
             pca_results = np.zeros((T, 3))
             for t in range(T):
                 
-                pca_results[t, :] = pca.fit_transform(beta[:, t, :])[k]
+                pca_results[t, :] = pca.fit_transform(alpha[:, t, :])[k]
             
             # Plot each topic's trajectory over time
             ax.plot(pca_results[:, 0], pca_results[:, 1], pca_results[:, 2], marker='o', label=f'Topic {k}')
